d1036254795208fb7d709330ddc94c9f5b545cbf
OleksandrBalashov <aleksandr.balashov.sax@gmai.com>
2023-12-10 16:38:40 +0100

    finish

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+class Key {
+  private signature = Math.random();
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person extends Key {
+  constructor(private key: Key) {
+    super();
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door = false;
+  protected key: Key;
+
+  comeIn(person: Person) {
+    const tenants: Person[] = [];
+
+    if (this.door) tenants.push(person);
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(protected key: Key) {
+    super();
+  }
+
+  public openDoor(key: Key) {
+    if (key === this.key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +49,3 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
-
-export {};
